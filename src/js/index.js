51b4f8f976709c3f7cb48a9564a928f767b02137
Kamil Lachowicz <kamil109111@gmail.com>
2021-10-14 21:02:07 +0200

    homework week 10

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,35 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import { registerSW } from "./pwa.js";
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const glassCounter = document.querySelector(".glassCounter--js");
+const addGlass = document.querySelector(".addGlass--js");
+const deleteGlass = document.querySelector(".deleteGlass--js");
+const key = new Date().toISOString().slice(0, 10);
+
+  let glass = localStorage.getItem(`${key}`);
+  if (glass) {
+    glassCounter.innerHTML = glass;
+  } else {
+    localStorage.setItem(`${key}`, 0);    
+    glassCounter.innerHTML = 0;    
+  }  
+
+  addGlass.addEventListener('click', () => {
+      glass++;     
+      localStorage.setItem(`${key}`, glass); 
+      glassCounter.innerHTML = glass;
+  })
+
+  deleteGlass.addEventListener('click', () => {
+    if (glass > 0){
+    glass--;    
+    localStorage.setItem(`${key}`, glass); 
+    glassCounter.innerHTML = glass;
+    }   
+})
+  
